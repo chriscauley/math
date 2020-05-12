a17dd2f202cc063fdabb5b24f419ab1f0d66dd37
chriscauley <chris@lablackey.com>
2020-05-12 20:46:42 +0000

    adding initial client (moving cos.js over from yacs repo)

---
+++ client/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route } from 'react-router-dom'
+
+import Home from './Home'
+import Nav from './Nav'
+import Cos from './cos'
+
+const App = () => {
+  return (
+    <>
+      <HashRouter>
+        <Nav />
+        <div className="app-content">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cos/" component={Cos} />
+        </div>
+      </HashRouter>
+    </>
+  )
+}
+
+const domContainer = document.querySelector('#react-app')
+ReactDOM.render(<App />, domContainer)
