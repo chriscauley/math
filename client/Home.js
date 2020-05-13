86dce82bdb3ff5130c9d00ec86bd8adc44f46419
chriscauley <chris@lablackey.com>
2020-05-13 14:37:54 +0000

    home link and first build

--- client/Home.js
+++ client/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  return <div className="flex">Wooo!!</div>
+  return (
+    <div className="flex">
+      <Link to="/cos/">Taylor Series Visualizer</Link>
+    </div>
+  )
 }
